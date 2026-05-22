1033aa1a364d6ca9b2618f49b0c0fd73a1cb8ba5
yuexin1i <yuexin0086@gmail.com>
2026-05-22 12:12:55 +0800

    fix api

--- src/calculate.py
+++ src/calculate.py
@@ -14,11 +14,13 @@ def mut(a: float, b: float) -> float:
     # return "123"
     return a * b
 
+
 @app.get("/")
 def home():
     return {"status":"Online","message":"這是簡易計算機API"}
 
+
 @app.get("/add")
 def calculate_add(a:float, b:float):
-    result = add_func(a,b)
-    return {"operation": "addition","a":a, "b":b, "result":result}
+    result = add_func(a, b)
+    return {"operation": "addition", "a": a, "b": b, "result": result}
